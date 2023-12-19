b6dca08412463d16182dcbb9389f539d51cac0ef
CCherry07 <2405693142@qq.com>
2023-12-19 20:56:50 +0800

    chore: update type

--- src/vite.ts
+++ src/vite.ts
@@ -2,4 +2,4 @@ import type { Plugin } from 'vite'
 import type { Options } from './types'
 import unplugin from '.'
 
-export default unplugin.vite as (options?: Options | undefined) => Plugin
+export default unplugin.vite as (options?: Options) => Plugin<any>
--- src/webpack.ts
+++ src/webpack.ts
@@ -1,3 +1,4 @@
-import unplugin from '.'
+import { WebpackPluginInstance } from 'unplugin'
+import unplugin, { Options } from '.'
 
-export default unplugin.webpack
+export default unplugin.webpack as (options?: Options) => WebpackPluginInstance
